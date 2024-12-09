1c7b23c277274a13d44b50ce298fe385d41dd870
Takumasa Hirabayashi <26487896+himanago@users.noreply.github.com>
2024-12-09 09:19:52 +0000

    Flex Message 修正

--- src/App.jsx
+++ src/App.jsx
@@ -151,126 +151,130 @@ function App() {
     if (liff.isApiAvailable("shareTargetPicker")) {
       liff.shareTargetPicker([
         {
-          "type": "bubble",
-          "hero": {
-            "type": "image",
-            "url": "https://raw.githubusercontent.com/himanago/miniapp-sample/refs/heads/main/icon.png",
-            "size": "full",
-            "aspectRatio": "20:13",
-            "aspectMode": "cover"
-          },
-          "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-              {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": `フルーツキャッチゲームで${score}点をとったよ！`,
-                    "size": "lg",
-                    "color": "#000000",
-                    "weight": "bold",
-                    "wrap": true
-                  }
-                ],
-                "spacing": "none"
-              },
-              {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "手軽に遊べるミニゲーム",
-                    "size": "sm",
-                    "color": "#999999",
-                    "wrap": true
-                  }
-                ],
-                "spacing": "none"
-              },
-              {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [
-                  {
-                    "type": "button",
-                    "action": {
-                      "type": "uri",
-                      "label": "遊んでみる！",
-                      "uri": `https://miniapp.line.me/${import.meta.env.VITE_LIFF_ID}`
-                    },
-                    "style": "primary",
-                    "height": "md",
-                    "color": "#17c950"
-                  },
-                  {
-                    "type": "button",
-                    "action": {
-                      "type": "uri",
-                      "label": "シェアする",
-                      "uri": `https://miniapp.line.me/${import.meta.env.VITE_LIFF_ID}/share`
-                    },
-                    "style": "link",
-                    "height": "md",
-                    "color": "#469fd6"
-                  }
-                ],
-                "spacing": "xs",
-                "margin": "lg"
-              }
-            ],
-            "spacing": "md"
-          },
-          "footer": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-              {
-                "type": "separator",
-                "color": "#f0f0f0"
-              },
-              {
-                "type": "box",
-                "layout": "horizontal",
-                "contents": [
-                  {
-                    "type": "image",
-                    "url": "https://raw.githubusercontent.com/himanago/miniapp-sample/refs/heads/main/icon.png",
-                    "flex": 1,
-                    "gravity": "center"
-                  },
-                  {
-                    "type": "text",
-                    "text": "フルーツキャッチゲーム",
-                    "flex": 19,
-                    "size": "xs",
-                    "color": "#999999",
-                    "weight": "bold",
-                    "gravity": "center",
-                    "wrap": false
-                  },
-                  {
-                    "type": "image",
-                    "url": "https://vos.line-scdn.net/service-notifier/footer_go_btn.png",
-                    "flex": 1,
-                    "gravity": "center",
-                    "size": "xxs",
-                    "action": {
-                      "type": "uri",
-                      "label": "action",
-                      "uri": `https://miniapp.line.me/${import.meta.env.VITE_LIFF_ID}`
+          "type": "flex",
+          "altText": "フルーツキャッチゲームのスコアをシェア！",
+          "contents": {
+            "type": "bubble",
+            "hero": {
+              "type": "image",
+              "url": "https://raw.githubusercontent.com/himanago/miniapp-sample/refs/heads/main/icon.png",
+              "size": "full",
+              "aspectRatio": "20:13",
+              "aspectMode": "cover"
+            },
+            "body": {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "box",
+                  "layout": "vertical",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": `フルーツキャッチゲームで${score}点をとったよ！`,
+                      "size": "lg",
+                      "color": "#000000",
+                      "weight": "bold",
+                      "wrap": true
                     }
-                  }
-                ],
-                "flex": 1,
-                "spacing": "md",
-                "margin": "md"
-              }
-            ]
+                  ],
+                  "spacing": "none"
+                },
+                {
+                  "type": "box",
+                  "layout": "vertical",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "手軽に遊べるミニゲーム",
+                      "size": "sm",
+                      "color": "#999999",
+                      "wrap": true
+                    }
+                  ],
+                  "spacing": "none"
+                },
+                {
+                  "type": "box",
+                  "layout": "vertical",
+                  "contents": [
+                    {
+                      "type": "button",
+                      "action": {
+                        "type": "uri",
+                        "label": "遊んでみる！",
+                        "uri": `https://miniapp.line.me/${liff.id}`
+                      },
+                      "style": "primary",
+                      "height": "md",
+                      "color": "#17c950"
+                    },
+                    {
+                      "type": "button",
+                      "action": {
+                        "type": "uri",
+                        "label": "シェアする",
+                        "uri": `https://miniapp.line.me/${liff.id}/share`
+                      },
+                      "style": "link",
+                      "height": "md",
+                      "color": "#469fd6"
+                    }
+                  ],
+                  "spacing": "xs",
+                  "margin": "lg"
+                }
+              ],
+              "spacing": "md"
+            },
+            "footer": {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "separator",
+                  "color": "#f0f0f0"
+                },
+                {
+                  "type": "box",
+                  "layout": "horizontal",
+                  "contents": [
+                    {
+                      "type": "image",
+                      "url": "https://raw.githubusercontent.com/himanago/miniapp-sample/refs/heads/main/icon.png",
+                      "flex": 1,
+                      "gravity": "center"
+                    },
+                    {
+                      "type": "text",
+                      "text": "フルーツキャッチゲーム",
+                      "flex": 19,
+                      "size": "xs",
+                      "color": "#999999",
+                      "weight": "bold",
+                      "gravity": "center",
+                      "wrap": false
+                    },
+                    {
+                      "type": "image",
+                      "url": "https://vos.line-scdn.net/service-notifier/footer_go_btn.png",
+                      "flex": 1,
+                      "gravity": "center",
+                      "size": "xxs",
+                      "action": {
+                        "type": "uri",
+                        "label": "action",
+                        "uri": `https://miniapp.line.me/${liff.id}`
+                      }
+                    }
+                  ],
+                  "flex": 1,
+                  "spacing": "md",
+                  "margin": "md"
+                }
+              ]
+            }
           }
         }
       ]).then(function (res) {
